8687f82c50830fd79a69c2dd60fc1ded8ae4baaf
khgf <45781723+khgf@users.noreply.github.com>
2019-01-02 08:09:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,24 @@
-client.on('ready', function(){    
+const Discord = require('discord.js');
 
-    var ms = 40000 ;    
+const client = new Discord.Client();
 
-    var setGame = ['Test...'];    
+client.on('ready', () => {
 
-    var i = -1;    
-
-    var j = 0;    
-
-    setInterval(function (){    
-
-        if( i == -1 ){    
-
-j = 1;    
-
-       }    
-
-        if( i == (setGame.length)-1 ){    
-
-            j = -1;    
-
-      }    
-
-       i = i+j;    
-
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
-
-}, ms);    
-
-    
+  console.log(`Logged in as ${client.user.tag}!`);
 
 });
+
+client.on('message', msg => {
+
+  if (msg.content === 'السلام عليكم') {
+
+    msg.reply('وعليكم السلام ورحمة الله وبركاته');
+
+  }
+
+});
+
+
 client.on('ready', function(){    
 
     var ms = 40000 ;    
@@ -64,3 +52,6 @@ j = 1;
     
 
 });
+
+
+client.login('NTIyOTEwOTU2NDk0NTIwMzIy.Dw3EyA.cOueXzdzPpW4XRLP0_SXdehspu8');
