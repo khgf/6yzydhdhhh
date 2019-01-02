e1ebbb3544a87b4a39d82c5ad6908ce6e518582c
khgf <45781723+khgf@users.noreply.github.com>
2019-01-02 06:57:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,33 @@
+client.on('ready', function(){    
 
+    var ms = 40000 ;    
+
+    var setGame = ['Test...'];    
+
+    var i = -1;    
+
+    var j = 0;    
+
+    setInterval(function (){    
+
+        if( i == -1 ){    
+
+j = 1;    
+
+       }    
+
+        if( i == (setGame.length)-1 ){    
+
+            j = -1;    
+
+      }    
+
+       i = i+j;    
+
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
+
+}, ms);    
+
+    
+
+});
