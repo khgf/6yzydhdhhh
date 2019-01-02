84150e2ef32236dc08657fa7efad34fc30f3e310
khgf <45781723+khgf@users.noreply.github.com>
2019-01-02 08:30:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,39 +19,4 @@ client.on('message', msg => {
 });
 
 
-client.on('ready', function(){    
-
-    var ms = 40000 ;    
-
-    var setGame = ['by Fras#9999','-help'];    
-
-    var i = -1;    
-
-    var j = 0;    
-
-    setInterval(function (){    
-
-        if( i == -1 ){    
-
-j = 1;    
-
-       }    
-
-        if( i == (setGame.length)-1 ){    
-
-            j = -1;    
-
-      }    
-
-       i = i+j;    
-
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
-
-}, ms);    
-
-    
-
-});
-
-
 client.login('NTIyOTEwOTU2NDk0NTIwMzIy.Dw3EyA.cOueXzdzPpW4XRLP0_SXdehspu8');
