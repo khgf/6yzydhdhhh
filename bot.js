7ee4b536241c8adb31ecb0a8716eaaa962ba6139
khgf <45781723+khgf@users.noreply.github.com>
2019-02-17 01:01:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,19 +4,43 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('By Hamo');
 
 });
 
-client.on('message', msg => {
+const sWlc = {}
 
-  if (msg.content === 'السلام عليكم') {
+client.on('message', message => {
 
-    msg.reply('وعليكم السلام ورحمة الله وبركاته');
+var prefix = "$";
+
+if(message.channel.type === "dm") return;
+
+if(message.author.bot) return;
+
+  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
+
+    channel: "chat"
+
+}
+
+const channel = sWlc[message.guild.id].channel
+
+  if (message.content.startsWith(prefix + "setwelcome")) {
+
+    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+
+    let newChannel = message.content.split(' ').slice(1).join(" ")
+
+    if(!newChannel) return message.reply(`**$setwelcome رجاء كتابت اسم الروم**`)
+
+    sWlc[message.guild.id].channel = newChannel
+
+    message.channel.send(`**${newChannel} تم عمل الترحيب في روم ${message.guild.name}**`);
 
   }
 
 });
 
 
-client.login('token');
+client.login('process.BOT.TOKEN');
